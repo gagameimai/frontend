d0c130b4d54b5ce67daf12cd42a8cb0257dab732
jameshung <jameshung@via.com.tw>
2022-08-09 11:29:15 +0800

    Add axios libary and the 常見問題 get data from api.

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,8 +33,9 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src:"~/plugins/aos.js", mode:"client" },
-    "~/plugins/vueAgile.js"
+    "~/plugins/vueAgile.js",
     // { src: '~/plugins/vueAgile.js', mode:"client" }
+    { src:"~/plugins/axios.js", mode:"client" },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
